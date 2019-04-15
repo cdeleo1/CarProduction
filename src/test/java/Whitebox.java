60238ef229564c5ebaaa7bc537830ebaee875ac1
Cristi DeLeo <cdeleo1@asu.edu>
2019-04-15 00:24:25 -0400

    Added Whitebox test file

---
+++ src/test/java/Whitebox.java
@@ -0,0 +1,75 @@
+package test.java;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import main.java.builder.*;
+import main.java.builder.Car.CarBuilder;
+
+public class Whitebox {
+	
+	Car basicCar;
+	
+	@Before
+	public void setUp() throws Exception {
+		
+		basicCar = new CarBuilder("Mazda", "RX8", 
+        		"Transit Mazda", "Red", "Sport", 0, "Leather", "Manual", 22.0)
+				.build();
+	}
+	
+	@Test
+	public void testGetMake() {
+		String result = basicCar.getMake();
+		assertTrue(result == "Mazda");
+	}
+	
+	@Test
+	public void testGetModel() {
+		String result = basicCar.getModel();
+		assertTrue(result == "RX8");
+	}
+	
+	@Test
+	public void testGetDealership() {
+		String result = basicCar.getDealership();
+		assertTrue(result == "Transit Mazda");
+	}
+	
+	@Test
+	public void testGetColor() {
+		String result = basicCar.getColor();
+		assertTrue(result == "Red");
+	}
+	
+	@Test
+	public void testGetVehicleType() {
+		String result = basicCar.getVehicleType();
+		assertTrue(result == "Sport");
+	}
+	
+	@Test
+	public void testGetNumCylinders() {
+		int result = basicCar.getNumCylinders();
+		assertTrue(result == 0);
+	}
+	
+	@Test
+	public void testGetCabMaterial() {
+		String result = basicCar.getCabMaterial();
+		assertTrue(result == "Leather");
+	}
+	
+	@Test
+	public void testGetTransmission() {
+		String result = basicCar.getTransmission();
+		assertTrue(result == "Manual");
+	}
+	
+	@Test
+	public void testGetTireSize() {
+		double result = basicCar.getTireSize();
+		assertTrue(result == 22.0);
+	}
+}
